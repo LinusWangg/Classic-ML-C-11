b353d4847ae5fefdb4150c33395c24b00135ddf6
LinusWangg <441656989@qq.com>
2022-02-04 20:32:02 +0800

    改良了lstm在DQN使用时的经验回放，达到了十分好的效果

--- DRL/DQN_LSTM.py
+++ DRL/DQN_LSTM.py
@@ -1,3 +1,4 @@
+from os import times
 from typing import AsyncIterable
 import torch
 import torch.nn as nn
@@ -8,17 +9,20 @@ import gym
 class Net(nn.Module):
     def __init__(self, N_STATES, N_ACTIONS):
         super(Net, self).__init__()
-        self.lstm = nn.LSTM(N_STATES, 50, batch_first=True)
-        self.fc = nn.Linear(50, N_ACTIONS)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.lstm = nn.LSTM(N_STATES, 50, batch_first=True).to(self.device)
+        self.fc = nn.Linear(50, N_ACTIONS).to(self.device)
         
     def forward(self, x, batch_size):
-        out, (h_n, c_n) = self.lstm(x)
-        h = out[:, :, :].reshape(out.shape[0]*out.shape[1], out.shape[2])
+        out, (h_n, c_n) = self.lstm(x.to(self.device))
+        h = out[:, :, :]
+        h = h.reshape(out.shape[0]*out.shape[1], out.shape[2])
         actions_value = self.fc(h)
         return actions_value
     
 class DQN(object):
     def __init__(self, MEMORY_CAPACITY, N_STATES, N_ACTIONS, LR):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.eval_net, self.target_net = Net(N_STATES, N_ACTIONS), Net(N_STATES, N_ACTIONS)
 
         self.learn_step_counter = 0
@@ -27,12 +31,13 @@ class DQN(object):
         self.memory = []
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
         self.loss_func = nn.MSELoss()
+        self.timestep = 8
 
     def choose_action(self, x, EPSILON, ENV_A_SHAPE, N_ACTIONS):
         x = torch.unsqueeze(torch.FloatTensor(x), 0).reshape(1, 1, N_STATES)
 
         if np.random.uniform() < EPSILON:
-            actions_value = self.eval_net.forward(x, 1)
+            actions_value = self.eval_net.forward(x, 1).to("cpu")
             action = torch.max(actions_value, 1)[1].data.numpy()
             action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)
 
@@ -57,33 +62,33 @@ class DQN(object):
         self.learn_step_counter += 1
 
         sample_index = np.random.choice(len(self.memory), BATCH_SIZE)
-        loss = 0
         for i in sample_index:
+            criteria = nn.MSELoss()
             b_memory = self.memory[i]
             size = len(b_memory[0])
+            index = np.random.randint(0, size-self.timestep+1)
 
-            b_s = torch.FloatTensor(b_memory[0]).reshape(1, size, N_STATES)
-            b_a = torch.LongTensor(b_memory[1][0]).reshape(len(b_memory[1][0]), 1)
-            b_r = torch.FloatTensor(b_memory[1][1]).reshape(len(b_memory[1][1]), 1)
-            b_s_ = torch.FloatTensor(b_memory[2]).reshape(1, size, N_STATES)
+            b_s = torch.FloatTensor(b_memory[0][index:index+self.timestep]).reshape(1, self.timestep, N_STATES)
+            b_a = torch.LongTensor(b_memory[1][0][index:index+self.timestep]).reshape(self.timestep, 1).to(self.device)
+            b_r = torch.FloatTensor(b_memory[1][1][index:index+self.timestep]).reshape(self.timestep, 1).to(self.device)
+            b_s_ = torch.FloatTensor(b_memory[2][index:index+self.timestep]).reshape(1, self.timestep, N_STATES)
 
             q_eval = self.eval_net.forward(b_s, 1).gather(1, b_a)
             q_next = self.target_net.forward(b_s_, 1).detach()
-            q_target = b_r + GAMMA*q_next.max(1)[0].view(size, 1)
-            loss += self.loss_func(q_eval, q_target)
+            q_target = b_r + GAMMA*q_next.max(1)[0].view(self.timestep, 1)
+            loss = criteria(q_eval, q_target)
 
-        loss /= BATCH_SIZE
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
 # Hyper Parameters
-BATCH_SIZE = 32
+BATCH_SIZE = 8
 LR = 0.01                   # learning rate
-EPSILON = 0.5               # greedy policy
+EPSILON = 0.9               # greedy policy
 GAMMA = 0.9                 # reward discount
 TARGET_REPLACE_ITER = 100   # target update frequency
-MEMORY_CAPACITY = 200
+MEMORY_CAPACITY = 50
 env = gym.make('CartPole-v0')
 env = env.unwrapped
 N_ACTIONS = env.action_space.n
@@ -92,9 +97,7 @@ ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(), int) else env.action_sp
 
 dqn = DQN(MEMORY_CAPACITY, N_STATES, N_ACTIONS, LR)
 
-for i in range(1000):
-    if i>500:
-        EPSILON = 0.9
+for i in range(10000):
     s = env.reset()
     ep_r = 0
     s_episode = []
@@ -108,13 +111,14 @@ for i in range(1000):
         a_episode.append(a)
 
         s_, r, done, info = env.step(a)
-        r_episode.append(r)
+        
         s__episode.append(s_)
 
         x, x_dot, theta, theta_dot = s_
         r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
         r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
         r = r1 + r2
+        r_episode.append(r)
 
 
         ep_r += r
